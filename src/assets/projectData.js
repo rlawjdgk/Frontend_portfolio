75e610cc5d2cfb78302cd512b8bfb28cedb39c31
김정하 <gimjeongha@gimjeonghaui-MacBookPro.local>
2024-12-19 21:11:04 +0900

    241219

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -38,28 +38,4 @@ export const ProjectData = [
     gitURL:
       "https://github.com/rlawjdgk/Frontend-Developer_Project/tree/main/Mbti",
   },
-  {
-    id: 4,
-    category: "frontend",
-    name: "Playlist",
-    image: "/img/background.jpg",
-    skillFirst: "/img/react.png",
-    skillSecond: null,
-    skillThird: null,
-    desc: "React를 기반으로 한 동적인 웹 애플리케이션으로, 사용자는 음악을 검색하고, 좋아하는 곡을 플레이리스트에 추가할 수 있습니다. 사용자의 경험을 개선하기 위해 다양한 검색 필터와 곡을 미리 들을 수 있는 기능을 제공하며, 직관적인 UI로 누구나 손쉽게 음악을 즐길 수 있도록 설계되었습니다.",
-    finalURL: "https://googleloginsignuppage.netlify.app",
-    gitURL: "",
-  },
-  {
-    id: 5,
-    category: "frontend",
-    name: "MBTI 검사",
-    image: "/img/background.jpg",
-    skillFirst: "/img/react.png",
-    skillSecond: null,
-    skillThird: null,
-    desc: "사용자는 미리 정의된 질문에 답하고, 그에 따른 결과를 실시간으로 확인할 수 있습니다. 이 애플리케이션은 React로 개발되었으며, 직관적인 UI를 통해 사용자가 쉽고 빠르게 검사 결과를 얻을 수 있도록 설계되었습니다. 또한, 사용자의 성격 유형 결과는 간단한 설명과 함께 제공되어, 각 성격 유형에 대한 이해를 돕습니다.",
-    finalURL: "https://googleloginsignuppage.netlify.app",
-    gitURL: "",
-  },
 ];
